2afeae6afc3fcba7ce2f978cf6cd039ae28ba97b
Pavel-Kirilenko <pavelkirilenko@mail.ru>
2024-12-04 11:17:08 +0400

    Добавить файл

---
+++ Header.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+/* функция слияния записи элементов в исходный массив */
+void merge(int* arr, int l, int m, int r);
+/* рекурсивная функция сортировки */
+void mergeSort(int* arr, int l, int r);
+
+// пос. Магистральный 04.12.2024
